81acb35efdb727242e02e7303665615b2134e226
John_Tolulope_Afariogun <afariogunjohn2502@gmailcom>
2022-09-26 11:49:35 -0700

    tested and run on my end, passed all checks

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - fills n number of memory space with constant byte b
+ * @s: pointer to memory space
+ * @b: byte to fill with
+ * @n: number of spaces to be filled
+ * Return: pointer
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int  i = 0;
+
+	while (i <= n)
+	{
+		*(s + i) = b;
+		i++;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,4 @@
+ # This contains code on:
+- multidimensional arrays and how to use them
+- pointers to pointers abd how to use them
+- most common C stabdard library functions to manipulate strings
