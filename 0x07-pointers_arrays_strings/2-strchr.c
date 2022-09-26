410a7b8db32758c2f710750a2e5dcbe2d79d1aa8
John_Tolulope_Afariogun <afariogunjohn2502@gmailcom>
2022-09-26 12:16:16 -0700

    ran successfully

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strchr - checks for characters and returns pointer to the character
+ * @s: pointer
+ * @c: character
+ * Return: returns pointer to the character
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; *(s + 1); i++)
+	{
+		if (*(s + i) == c)
+		{
+			return (s + i);
+		}
+	}
+	return (0);
+}
