96ede24a1023f57b2826ed549383db83c1513913
John_Tolulope_Afariogun <afariogunjohn2502@gmailcom>
2022-11-03 15:43:04 -0700

    testing

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,13 +3,13 @@
  * _strcpy - copies a string from a source to a destination
  * @dest: pointer to the destination
  * @src: pointer to the source
- * return: returns dest as the destination
+ * Return: @dest as the destination
  */
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	while (src[i] != '\0')
+	while (src[i])
 	{
 		dest[i] = src[i];
 		i++;
