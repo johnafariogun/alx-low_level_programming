4bb7f99a4c9789bfff69ef55146d9e627248b0f7
John_Tolulope_Afariogun <afariogunjohn2502@gmailcom>
2022-11-08 12:36:43 -0800

    i think i failed a betty check

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  * @argv: array of arguments
  * Return: Always 0 (on success)
  */
-int main (int __attribute__((__unused__)) argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
