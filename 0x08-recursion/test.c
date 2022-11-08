85ca1a0b12fd44328c7dc50b496a516a98ed0810
John_Tolulope_Afariogun <afariogunjohn2502@gmailcom>
2022-11-08 07:25:59 -0800

    learning, trying to catch up

---
+++ 0x08-recursion/test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int factorial(int n)
+{
+	if (n <= 1 )
+		return 1;
+	else
+	{
+		return n * factorial(n - 1);
+	}
+}
+int main(void)
+{
+	int fac;
+	fac = factorial(10);
+	printf("%d",fac);
+}
