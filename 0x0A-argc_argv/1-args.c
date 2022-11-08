8a67a5b7f74775e8bf48f80f847c11abef6dbae5
John_Tolulope_Afariogun <afariogunjohn2502@gmailcom>
2022-11-08 12:47:07 -0800

    learning

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point of the program
+ * @argc: number of arguments passed to the program
+ * @argv: array of arguments
+ * Return: Always 0 (on success)
+ */
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
