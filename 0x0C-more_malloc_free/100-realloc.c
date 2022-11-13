9589bae0041f96a4e22233dee4e3857bc2428d7f
John_Tolulope_Afariogun <afariogunjohn2502@gmailcom>
2022-11-13 05:11:08 -0800

    learning, follow the steps \

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _realloc - reallocates a arrayy to another
+ * @ptr: pointer to the array
+ * @old_size: old size of the array
+ * @new_size: new size of the array
+ * Return: pointer to a new array or NULL
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *mem;
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (ptr == NULL)
+	{
+		mem = malloc(new_size);
+
+		if (mem == NULL)
+			return (NULL);
+
+		return (mem);
+	}
+
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	mem = malloc(sizeof(ptr) * new_size);
+
+	if (mem == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+
+	free(ptr);
+	return (mem);
+}
