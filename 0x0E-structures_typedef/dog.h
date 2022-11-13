25b6e317222aad19852540543014b082598aa8a4
John_Tolulope_Afariogun <afariogunjohn2502@gmailcom>
2022-11-13 12:20:34 -0800

    learning

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a struct of a dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Description: struct called dog that stores a dog's name
+ * ,age, owner 
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
