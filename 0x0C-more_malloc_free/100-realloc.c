b5351897f2c71fed37d7fdaca2dfae223da5ce6e
John_Tolulope_Afariogun <afariogunjohn2502@gmailcom>
2022-11-13 05:18:48 -0800

    checkng

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -31,7 +31,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	}
 
-	mem = malloc(sizeof(ptr) * new_size);
+	mem = malloc(new_size);
 
 	if (mem == NULL)
 	{
