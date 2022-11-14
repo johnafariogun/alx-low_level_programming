5e22e733dd366df8af4c88bc8cebfdea02a35d29
John_Tolulope_Afariogun <afariogunjohn2502@gmailcom>
2022-11-14 06:39:46 -0800

    checking two at the same time, i am graudually becoming a pro

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints the nmae of the file and a new line
+ * Return: always 0 (on succcess)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
