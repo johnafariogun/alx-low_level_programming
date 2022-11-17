68f0e5d0464cac883cbdd7a0641c8ddced797919
John_Tolulope_Afariogun <afariogunjohn2502@gmailcom>
2022-11-17 07:18:29 -0800

    trying t force myself back above 80%

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - calculates the sum of all multiples of 3 and 5 below 1024
+ * Return: alqays 0
+ */
+int main(void)
+{
+	unsigned int sum = 0, num = 3;
+	while (num <= 1024)
+	{
+		if (num % 3 == 0 || num % 5 == 0)
+		{
+			sum += num;
+		}
+		num++;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
