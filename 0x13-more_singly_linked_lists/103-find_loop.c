28fb19a8039ebbdb21948312e13ea667c563d869
John_Tolulope_Afariogun <afariogunjohn2502@gmailcom>
2022-11-24 13:56:47 -0800

    done

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * find_listint_loop - finds the address that the loop for a linked list starts
+ * @head: the head node in the linked list
+ * Return: the address of the node, else NULL
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	while (head != NULL)
+	{
+		if (head <= head->next)
+			return (head);
+
+		head = head->next;
+	}
+	return (NULL);
+}
