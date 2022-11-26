c3d4795ec5f6781c5936b8db0cf3ec9d613af673
John_Tolulope_Afariogun <afariogunjohn2502@gmailcom>
2022-11-26 13:40:54 -0800

    done with mandatory tasks i think this is a combibnation of the previous 3 tasks along with the task 2

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -8,7 +8,7 @@
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int num = n ^ m;
-	int i = 0, index = ((sizeof(unsigned long int) * 8) -1);
+	int i = 0, index = ((sizeof(unsigned long int) * 8) - 1);
 
 	for (; index >= 0; index--)
 	{
