062188d127d6f1bd6001518cbe29401f9406ae34
John_Tolulope_Afariogun <afariogunjohn2502@gmailcom>
2022-12-02 06:20:22 -0800

    i think i will pass all checks now

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,7 +23,12 @@ int create_file(const char *filename, char *text_content)
 		;
 
 	result = write(f, text_content, i);
-	close(f);
+	if (result == -1)/* if the file wasn't written into successfully*/
+		return (-1);
+	result = close(f);
 
-	return (result);
+	if (result == -1)/*if the file wasn't closed successfully*/
+		return (-1);
+
+	return (1);
 }
