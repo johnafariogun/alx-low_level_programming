13e2aeb472b5f0e81dfb5772611ea33541c6cc1c
John_Tolulope_Afariogun <afariogunjohn2502@gmailcom>
2022-12-09 13:11:36 -0800

    actucally lost my code for a moment there

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_dnodeint - frees the dlistint_t list
+ *
+ * @head: pointer to the head of the doubly linked list
+ **/
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *i;
+
+	while (head)
+	{
+		i = head->next;
+		free(head);
+		head = i;
+	}
+}
